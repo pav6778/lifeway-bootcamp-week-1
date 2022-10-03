9756a3784be76bd9bb4f7e47c7932219a7129035
pav6778 <panchukovp@gmail.com>
2022-10-03 15:28:07 -0400

    week 1

---
+++ index.js
@@ -0,0 +1,16 @@
+// 9 boxes, x or o, possible wins 123 456 789, 147, 258, 369, 159, 357
+
+
+let arr = [
+    true,true,true,
+    false,false,true,
+    false,false,false
+]
+
+const newFunc = () => {
+    
+
+
+}
+
+newFunc()
